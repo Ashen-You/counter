bfb08fe58d0fc7280373cf2abf7aa405501bd3f7
Travis You <1244098000@qq.com>
2022-06-15 11:35:26 +0800

    feat: index use Counter and use reducer as store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore } from 'redux';
+import Counter from "./components/Counter";
+import counter from "./reducers";
 
 const rootEl = document.getElementById("root");
+const store = createStore(counter);
 
 const render = () => ReactDOM.render(
-    <div>
-
-    </div>,
+    <Counter
+        value={store.getState()}
+        onIncrement={() => store.dispatch({type : 'INCREMENT'})}
+        onDecrement={() => store.dispatch({type : 'DECREMENT'})}
+    />,
     rootEl)
 ;
 
 render();
+store.subscribe(render);
